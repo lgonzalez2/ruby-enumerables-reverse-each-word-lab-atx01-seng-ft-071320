2af301ccc2f319976227e35c684a133677574444
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-06 17:56:12 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,3 @@
-# First figure out how to turn the string into an array, separating each word into an element. 
-# Then use the reverse method on each individual element of the array, and shovel into a new string. 
-# Make sure the string returns!!!! 
-
 def reverse_each_word(sentence)
   sentence_array = sentence.split(/ /)
   reverse_sentence = ""
